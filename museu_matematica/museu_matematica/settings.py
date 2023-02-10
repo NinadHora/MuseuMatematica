1b2a89c0bed86e18fe88d10da8d0b5f39ae9587d
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2023-02-10 16:56:38 -0300

    deploy static

--- museu_matematica/museu_matematica/settings.py
+++ museu_matematica/museu_matematica/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'museu_matematica\static')]
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
